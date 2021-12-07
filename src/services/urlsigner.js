6f3cd6799ad97f62f6625a69c3ed43c170d44960
Ruben Murga <ruben.murga.d@gmail.com>
2021-12-07 00:26:09 -0600

    changing status code

--- src/services/urlsigner.js
+++ src/services/urlsigner.js
@@ -2,24 +2,29 @@ export default {
   getSignedURL(file, config) {
     let payload = {
       filePath: file.name,
-      contentType: file.type
-    }
+      contentType: file.type,
+    };
 
     return new Promise((resolve, reject) => {
       var fd = new FormData();
       let request = new XMLHttpRequest(),
-          signingURL = (typeof config.signingURL === "function") ?  config.signingURL(file) : config.signingURL;
+        signingURL =
+          typeof config.signingURL === "function"
+            ? config.signingURL(file)
+            : config.signingURL;
       request.open("GET", signingURL);
       request.onload = function () {
         if (request.status == 200) {
           resolve(JSON.parse(request.response));
         } else {
-          reject((request.statusText));
+          reject(request.statusText);
         }
       };
       request.onerror = function (err) {
-        console.error("Network Error : Could not send request to AWS (Maybe CORS errors)");
-        reject(err)
+        console.error(
+          "Network Error : Could not send request to AWS (Maybe CORS errors)"
+        );
+        reject(err);
       };
       if (config.withCredentials === true) {
         request.withCredentials = true;
@@ -36,11 +41,15 @@ export default {
     });
   },
   sendFile(file, config, is_sending_s3) {
-    var handler = (is_sending_s3) ? this.setResponseHandler : this.sendS3Handler;
+    var handler = is_sending_s3 ? this.setResponseHandler : this.sendS3Handler;
 
     return this.getSignedURL(file, config)
-      .then((response) => {return handler(response, file)})
-      .catch((error) => { return error; });
+      .then((response) => {
+        return handler(response, file);
+      })
+      .catch((error) => {
+        return error;
+      });
   },
   setResponseHandler(response, file) {
     file.s3Signature = response.signature;
@@ -53,36 +62,47 @@ export default {
     Object.keys(signature).forEach(function (key) {
       fd.append(key, signature[key]);
     });
-    fd.append('file', file);
+    fd.append("file", file);
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
-      request.open('PUT', response.postEndpoint);
+      request.open("PUT", response.postEndpoint);
       request.onload = function () {
-        if (request.status == 201) {
-          var s3Error = (new window.DOMParser()).parseFromString(request.response, "text/xml");
+        if (request.status == 200) {
+          var s3Error = new window.DOMParser().parseFromString(
+            request.response,
+            "text/xml"
+          );
           var successMsg = s3Error.firstChild.children[0].innerHTML;
           resolve({
-            'success': true,
-            'message': successMsg
-          })
+            success: true,
+            message: successMsg,
+          });
         } else {
-          var s3Error = (new window.DOMParser()).parseFromString(request.response, "text/xml");
+          var s3Error = new window.DOMParser().parseFromString(
+            request.response,
+            "text/xml"
+          );
           var errMsg = s3Error.firstChild.children[0].innerHTML;
           reject({
-            'success': false,
-            'message': errMsg + ". Request is marked as resolved when returns as status 201"
-          })
+            success: false,
+            message:
+              errMsg +
+              ". Request is marked as resolved when returns as status 201",
+          });
         }
       };
       request.onerror = function (err) {
-        var s3Error = (new window.DOMParser()).parseFromString(request.response, "text/xml");
+        var s3Error = new window.DOMParser().parseFromString(
+          request.response,
+          "text/xml"
+        );
         var errMsg = s3Error.firstChild.children[1].innerHTML;
         reject({
-          'success': false,
-          'message': errMsg
-        })
+          success: false,
+          message: errMsg,
+        });
       };
       request.send(fd);
     });
-  }
-}
+  },
+};
